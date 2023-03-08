a07d0194c759017642a3470de7d7252a3630980d
David Jandron <jandron1211@gmail.com>
2023-03-08 11:54:51 -0800

    BUGFIX run issues

---
+++ BestRestaurants/Controllers/CuisineTypesController.cs
@@ -0,0 +1,72 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using BestRestaurants.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BestRaurants.Controllers
+{
+  public class CuisineTypesController : Controller
+  {
+    private readonly BestRestaurantsContext _db;
+
+    public CuisineTypesController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<CuisineType> model = _db.CuisineType.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(CuisineType cuisineType)
+    {
+      _db.CuisineTypes.Add(cuisineType);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      CuisineType thisCuisineType = _db.CuisineTypes.Include(cuisineType => cuisineType.Restaurants).FirstOrDefault(cuisineType => cuisineType.CuisineTypeId == id);
+      return View(thisCuisineType);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      CuisineType thisCuisineType = _db.CuisineTypes.FirstOrDefault(cuisineType => cuisineType.CuisineTypeId == id);
+      return View(thisCuisineType);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(CuisineType cuisineType)
+    {
+      _db.CuisineTypes.Update(cuisineType);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      CuisineType thisCuisineType = _db.CuisineTypes.FirstOrDefault(cuisineType => cuisineType.CuisineTypeId == id);
+      return View(thisCuisineType);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      CuisineType thisCuisineType = _db.CuisineTypes.FirstOrDefault(cuisineType => cuisineType.CuisineTypeId == id);
+      _db.CuisineTypes.Remove(thisCuisineType);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
--- BestRestaurants/Models/CuisineType.cs
+++ BestRestaurants/Models/CuisineType.cs
@@ -2,10 +2,10 @@ using System.Collections.Generic;
 
 namespace BestRestaurants.Models
 {
-  public class Cuisine
+  public class CuisineType
   {
     public int CuisineId { get; set; }
-    public string CuisineType { get; set; }
+    public string Cuisine { get; set; }
     public List<Restaurant> Restaurants { get; set; }
   }
 }
